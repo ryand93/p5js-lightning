475183b377b1b3e352df618b85012b0b3095b991
Static Void <staticvoidnetwork@outlook.com>
2020-07-27 23:17:42 +0200

    commit initial files - mostly guesswork until now but working quite well

---
+++ Lightning.js
@@ -0,0 +1,117 @@
+var width = 900;
+var height = 500;
+
+
+
+var maxDTheta = PI/10;
+var minDTheta = PI/20;
+var maxTheta = PI/2;
+var childGenOdds = .01;
+
+var minBoltWidth = 3;
+var maxBoltWidth = 10;
+
+var minJumpLength = 1;
+var maxJumpLength = 10;
+
+var stormMode = true;
+var fadeStrikes = true;
+var randomColors = false;
+var maxTimeBetweenStrikes = 3000;
+
+//var yellow = var(59,99,99);
+//var red = var(0,99,99);
+var boltColor;
+var skyColor;
+
+
+var bolt;
+var lastStrike = 0;
+var nextStrikeInNms = 0;
+
+//var playThunder = false;
+//var useDing = false;
+//import ddf.minim.*;
+//AudioSample thunderSound;
+//Minim minim;
+
+//distance, in milliseconds, of the storm.
+var meanDistance = 0;
+//if the current time matches the time in this arraylist, it should fire!
+var thunderTimes = [];
+
+
+function setup(){
+  colorMode(HSB,100);
+  smooth();
+  createCanvas(windowWidth, windowHeight);
+  //minim = new Minim(this);
+  //thunderSound = minim.loadSample("thunder.mp3");
+  noFill();
+  meanDistance = 1000*.5;
+  
+//  yellow = var(60/3.6,99,99);
+//  red = var(0,99,99);
+  boltColor = color(0,0,99);
+  skyColor = color(0,0,0);
+  background(skyColor);
+
+  bolt = new lightningBolt(random(0,width),0,random(minBoltWidth,maxBoltWidth),0,minJumpLength,maxJumpLength,boltColor);
+}
+
+function draw(){
+  //check if any of the stored times need to make a 'ding'
+  //if(playThunder && thunderTimes.size() > 0)
+  //  if(millis() > (Float)thunderTimes.get(0)){
+  //    thunderTimes.remove(0);
+  //    thunderSound.trigger();
+  //    prvarln("boom!");
+  //  }
+  
+  if(stormMode && millis()-lastStrike>nextStrikeInNms){//time for a new bolt?
+    lastStrike = millis();
+    nextStrikeInNms = random(0,maxTimeBetweenStrikes);
+    
+    bolt = new lightningBolt(random(0,width),0,random(minBoltWidth,maxBoltWidth),0,minJumpLength,maxJumpLength,boltColor);
+    bolt.draw();
+    //if(playThunder)
+    //  thunderTimes.add(bolt.getThunderTime());
+  }
+  else{
+    if(fadeStrikes){
+      noStroke();
+      fill(skyColor);
+      rect(0,0,width,height);
+      noFill();
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function stop(){
+  //thunderSound.close();
+  //minim.stop(); 
+  // super().stop();
+}
+
+function randomSign() //returns +1 or -1
+{
+  var num = random(-1,1);
+  if(num==0)
+    return -1;
+  else
+    return (float)(num/abs(num));
+}
+
+function randomColor(){
+  return color(random(0,100),99,99);
+}
+
+function slightlyRandomColor( inputCol, length){
+  var h = hue(inputCol);
+  h = (h+random(-length,length))%100;
+  return color(h,99,99);
+}
---
+++ lightningBolt.js
@@ -0,0 +1,86 @@
+const PI=3.14;
+const HALF_PI=PI/2;
+
+class lightningBolt{
+  //  lineWidth0=0;
+  //  theta=0;
+  //  x0=0;
+  //  y0=0;
+  //  x1=0;
+  //  y1=0;
+  //  x2=0;
+  //  y2=0;
+  //  straightJump=0;
+  //  straightJumpMax=0;
+  //  straightJumpMin=0;
+  //  lineWidth=0;
+  //  myColor;
+  constructor( x0I,  y0I,  width0,  theta0,  jumpMin,  jumpMax,  inputColor){
+
+    this. lineWidth0 = width0;
+    this. lineWidth = width0;
+    this.theta = theta0;
+    this.x0 = x0I;
+    this.y0 = y0I;
+    this.x1 = x0I;
+    this. y1 = y0I;
+    this.x2 = x0I;
+    this.y2 = y0I;
+    this.straightJumpMin = jumpMin;
+    this.straightJumpMax = jumpMax;
+    this.myColor = inputColor;
+    //it's a wandering line that goes straight for a while,
+    //then does a jagged jump (large dTheta), repeats.
+    //it does not aim higher than thetaMax
+    //(where theta= 0 is down)
+    this.straightJump = random(this.straightJumpMin,this.straightJumpMax);
+  }
+
+  //tells when the thunder should sound.
+   getThunderTime(){
+    return (millis()+meanDistance*(1+random(-.1,.1)));
+  }
+
+   draw()
+  {
+    while(this.y2<height && (this.x2>0 && this.x2<width))
+    {
+      strokeWeight(1);
+      
+      this.theta += randomSign()*random(minDTheta, maxDTheta);
+      if(this.theta>maxTheta)
+      this.theta = maxTheta;
+      if(this.theta<-maxTheta)
+      this.theta = -maxTheta;
+        
+      this.straightJump = random(this.straightJumpMin,this.straightJumpMax);
+      this.x2 = this.x1-this.straightJump*cos(this.theta-HALF_PI);
+      this.y2 = this.y1-this.straightJump*sin(this.theta-HALF_PI);
+      
+      if(randomColors)
+      this.myColor = this.slightlyRandomColor(this.myColor,this.straightJump);
+      
+      this.lineWidth = map(this.y2, height,this.y0, 1,this.lineWidth0);
+      if(this.lineWidth<0)
+      this.lineWidth = 0;
+      stroke(this.myColor);
+      strokeWeight(this.lineWidth);
+      line(this.x1,this.y1,this.x2,this.y2);
+      this.x1=this.x2;
+      this.y1=this.y2;
+      
+      //think about making a fork
+      if(random(0,1)<childGenOdds){//if yes, have a baby!
+        var newTheta = this.theta;
+        newTheta += randomSign()*random(minDTheta, maxDTheta);
+        if(this.theta>maxTheta)
+        this.theta = maxTheta;
+        if(this.theta<-maxTheta)
+        this.theta = -maxTheta;
+//        nForks++;
+        (new lightningBolt(this.x2, this.y2, this.lineWidth, newTheta, this.straightJumpMin, this.straightJumpMax,boltColor)).draw();
+        //it draws the whole limb before continuing.
+      }
+    }
+  }
+}
